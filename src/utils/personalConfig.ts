1b7cbb71c9fd5b141c7aa53ca1b584a11415fc18
dacmarcell <dacmarcell@gmail.com>
2025-01-03 19:49:34 -0300

    Rename config file to personalConfig and add email link to social links

--- src/utils/personalConfig.ts
+++ src/utils/personalConfig.ts
@@ -1,6 +1,9 @@
-export const config = {
+const personalConfig = {
   social_links: {
     github: "https://github.com/dacmarcell",
     linkedin: "https://linkedin.com/in/marcell-dactes",
+    email: "mailto:devmarcell@protonmail.com",
   },
 };
+
+export default personalConfig;
