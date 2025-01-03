5d4362056fd932dd42d3118f30354412fe735477
dacmarcell <dacmarcell@gmail.com>
2025-01-03 19:49:40 -0300

    Add AboutMe component to showcase personal background and skills

---
+++ src/components/AboutMe.tsx
@@ -0,0 +1,20 @@
+export default function AboutMe() {
+  return (
+    <>
+      <h1 className="text-4xl font-semibold  mt-16 mb-8">Sobre mim</h1>
+      <p className="text-lg">
+        Bacharelando em Ciência da Computação, Desenvolvedor Web FullStack com
+        aproximadamente 1 ano de experiência. Aprecio conceitos como DRY, SOLID,
+        KISS e Clean Code durante a codificação de aplicações, com foco no
+        desenvolvimento de aplicações escaláveis e performáticas, utilizando
+        tecnologias da atualidade. Como um bom Júnior, também adoro experimentar
+        novas tecnologias. Atualmente trabalho com o ecossistema Typescript com
+        foco na Web, utilizando Next.js e Node.js. No back-end que é minha
+        stack, atualmente utilizo C# e .NET para criação de aplicações. Também
+        tenho projetos pessoais em Rust e Python. Tenho um gato chamado Django e
+        amo pizza. Sou curioso e inquieto. Sempre estudando algo novo ou fazendo
+        algum projeto.
+      </p>
+    </>
+  );
+}
