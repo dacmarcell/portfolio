d8a910e000d50bd7820aa4f68aede8485a2416af
dacmarcell <dacmarcell@gmail.com>
2025-01-03 22:46:26 -0300

    Add Line component for improved visual separation in layouts

---
+++ src/components/ui/Line.tsx
@@ -0,0 +1,18 @@
+export default function Line() {
+  return (
+    <hr
+      className="
+          w-1/2 
+          my-8 
+          border-t-2 
+          border-solid 
+          border-foreground 
+          dark:border-background 
+          rounded-full 
+          sm:w-1/4 
+          sm:my-12 
+          dark:border-opacity-50
+        "
+    />
+  );
+}
