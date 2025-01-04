036ddd206859044afdab4648cbff16278c42857e
dacmarcell <dacmarcell@gmail.com>
2025-01-03 22:03:01 -0300

    Integrate ProjectsList component into the projetos page for improved project display

--- src/app/projetos/page.tsx
+++ src/app/projetos/page.tsx
@@ -1,3 +1,9 @@
+import ProjectsList from "@/components/ProjectsList";
+
 export default function Page() {
-  return <h1>projetos</h1>;
+  return (
+    <div className="flex flex-col items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <ProjectsList />
+    </div>
+  );
 }
