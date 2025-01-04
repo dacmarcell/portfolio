8a00a79c0500f6a967894d398204835cefd329a9
dacmarcell <dacmarcell@gmail.com>
2025-01-03 22:02:52 -0300

    Add filtering for favorite projects in ProjectsList component

--- src/components/ProjectsList.tsx
+++ src/components/ProjectsList.tsx
@@ -1,13 +1,27 @@
+import { useMemo } from "react";
 import { projects } from "../mocks/projects.json";
 import Card from "./Card";
 import Title from "./ui/Title";
+import Link from "next/link";
 
-export default function ProjectsList() {
+interface ProjectsListProps {
+  onlyFavorites?: boolean;
+}
+
+export default function ProjectsList(props: ProjectsListProps) {
+  const { onlyFavorites } = props;
+
+  const filteredProjects = useMemo(() => {
+    return onlyFavorites
+      ? projects.filter((project) => project.isFavorite)
+      : projects;
+  }, [onlyFavorites]);
+
   return (
     <div>
       <Title>Projetos</Title>
       <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
-        {projects.map((project) => {
+        {filteredProjects.map((project) => {
           return (
             <Card
               isProject
@@ -18,6 +32,23 @@ export default function ProjectsList() {
             />
           );
         })}
+        {onlyFavorites ? (
+          <Link
+            href="/projetos"
+            className="
+          text-lg
+          text-foreground
+          hover:text-accent 
+          dark:hover:text-accent 
+          underline
+          cursor-pointer
+          text-black
+          dark:text-white
+        "
+          >
+            Ver todos
+          </Link>
+        ) : null}
       </div>
     </div>
   );
