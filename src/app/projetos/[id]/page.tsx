d1d9229aad6264f893ba088797c9cca6fa04d16a
dacmarcell <dacmarcell@gmail.com>
2025-01-03 22:46:14 -0300

    Fetch project details by ID and update page layout with project title and description

--- src/app/projetos/[id]/page.tsx
+++ src/app/projetos/[id]/page.tsx
@@ -1,13 +1,57 @@
+import Link from "next/link";
+import { projects } from "../../../mocks/projects.json";
+import Title from "@/components/ui/Title";
+
+async function getProjectByID(id: number) {
+  return isNaN(id) ? null : projects.find((project) => project.id === id);
+}
+
 export default async function Page({
   params,
 }: {
   params: Promise<{ id: string }>;
 }) {
   const { id } = await params;
+  const project = await getProjectByID(parseInt(id));
+
+  if (!project) {
+    return (
+      <div>
+        <h1>Projeto não encontrado</h1>
+        <Link href="/">Voltar</Link>
+      </div>
+    );
+  }
 
   return (
     <div>
-      <h1>Projeto {id}</h1>
+      <Title>{project.title}</Title>
+      <p
+        className="
+        text-foreground 
+        dark:text-white
+        text-black
+        text-lg 
+        sm:text-xl
+        my-4
+      "
+      >
+        {project.description}
+      </p>
+      <Link
+        href="/"
+        className="
+        text-foreground 
+        dark:text-white
+        text-black
+        text-lg 
+        sm:text-xl
+        my-4
+        hover:underline
+      "
+      >
+        Voltar
+      </Link>
     </div>
   );
 }
