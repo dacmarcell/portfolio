be4629073b4bcab122f855de08ba61199fdc7631
dacmarcell <dacmarcell@gmail.com>
2025-01-03 22:46:18 -0300

    Update Footer component background color for improved visibility in dark mode

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="bg-gray-900 text-white py-4 sticky top-0 z-50">
+    <footer className="dark:bg-gray-900 bg-slate-950 text-white py-4 sticky top-0 z-50">
       <div className="container mx-auto px-4 flex justify-between items-center text-center md:text-left">
         <p>Dev Marcell &copy; {new Date().getFullYear()}</p>
       </div>
