af366f7e1b63a137b08e000cddb1ecfda587e8a5
dacmarcell <dacmarcell@gmail.com>
2025-01-03 22:46:11 -0300

    Simplify Page component by removing unnecessary layout wrapper around ProjectsList

--- src/app/projetos/page.tsx
+++ src/app/projetos/page.tsx
@@ -1,9 +1,5 @@
 import ProjectsList from "@/components/ProjectsList";
 
 export default function Page() {
-  return (
-    <div className="flex flex-col items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <ProjectsList />
-    </div>
-  );
+  return <ProjectsList />;
 }
