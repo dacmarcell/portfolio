0fa5f45721438d05520d918ecc635b0aa24946e9
dacmarcell <dacmarcell@gmail.com>
2025-01-05 14:41:57 -0300

    feat: fetch and display repository commits in the Page component

--- src/app/repositorios/repositorio/page.tsx
+++ src/app/repositorios/repositorio/page.tsx
@@ -1,25 +1,52 @@
 "use client";
 
+import CustomLink from "@/components/ui/CustomLink";
 import { useRepositoryContext } from "@/contexts/RepositoryContext";
-import Link from "next/link";
+import { useEffect, useState } from "react";
+
+type Commit = {
+  sha: string;
+};
 
 export default function Page() {
+  const [commits, setCommits] = useState<Array<Commit>>([]);
   const { foundRepository } = useRepositoryContext();
+
+  useEffect(() => {
+    const getFoundRepositoryCommits = async (commitsEndpoint: string) => {
+      const res = await fetch(commitsEndpoint);
+      return await res.json();
+    };
+
+    if (foundRepository?.commits_url) {
+      getFoundRepositoryCommits(foundRepository.commits_url).then(setCommits);
+    }
+  }, [foundRepository]);
 
   if (!foundRepository) {
     return (
       <>
         <h1>Nenhum repositório encontrado.</h1>
-        <Link href="/repositorios">Voltar</Link>
+        <CustomLink href="/repositorios">Voltar</CustomLink>
       </>
     );
   }
 
   return (
-    <div>
+    <>
       <h1>{foundRepository.id}</h1>
       <h1>{foundRepository.name}</h1>
-      <Link href="/repositorios">Voltar</Link>
-    </div>
+      <h1>{foundRepository.description}</h1>
+      <h1>{foundRepository.language}</h1>
+      <h1>{foundRepository.topics.toString()}</h1>
+      <div>
+        {commits.map((commit) => {
+          return <h1 key={commit.sha}>{commit.sha}</h1>;
+        })}
+      </div>
+      <h1>{foundRepository.created_at.toString()}</h1>
+      <h1>{foundRepository.updated_at.toString()}</h1>
+      <CustomLink href="/repositorios">Voltar</CustomLink>
+    </>
   );
 }
