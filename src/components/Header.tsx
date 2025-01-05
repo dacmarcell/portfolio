ac8ebf118922e6de82848d3e92e525784f2eafb6
dacmarcell <dacmarcell@gmail.com>
2025-01-04 23:35:17 -0300

    Refactor Header component to include Logo and enhance styling for improved accessibility

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,28 +3,21 @@ import Link from "next/link";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 import { navigation } from "../mocks/navigation.json";
 import { MdEmail } from "react-icons/md";
+import Logo from "./Logo";
 
 export default function Header() {
   return (
-    <header className="dark:bg-gray-900 bg-slate-950 text-white py-4 sticky top-0 z-50">
-      {/* Header container */}
-      <div className="container mx-auto px-4 flex justify-between items-center">
-        {/* Website title */}
-        <div>
-          <h1 className="text-lg font-semibold">Marcell</h1>
-          <h2>Fullstack web dev</h2>
-        </div>
-        {/* Navigation menu */}
+    <header className="dark:bg-gray-950 bg-green-500 text-white py-4 sticky top-0 z-50">
+      <div className="container mx-auto px-4 flex justify-around items-center">
+        <Logo />
         <nav className="hidden md:block">
           <ul className="flex gap-x-6">
             <NavigationLinks />
           </ul>
         </nav>
-        {/* Social media icons */}
         <div className="hidden md:block">
           <SocialIcons />
         </div>
-        {/* Add Mobile Navigation Toggle Here */}
       </div>
     </header>
   );
@@ -34,7 +27,10 @@ function NavigationLinks() {
   return navigation.map((item) => {
     return (
       <li key={item.id}>
-        <Link href={item.endpoint} className="hover:text-gray-300">
+        <Link
+          href={item.endpoint}
+          className="hover:text-green-800 text-black dark:text-white"
+        >
           {item.formattedName}
         </Link>
       </li>
@@ -42,33 +38,30 @@ function NavigationLinks() {
   });
 }
 
-// Define the SocialIcons component
 function SocialIcons() {
   return (
     <div className="flex gap-x-4">
-      {/* GitHub icon */}
       <a
         href={personalConfig.social_links.github}
         target="_blank"
         rel="noopener noreferrer"
       >
-        <FaGithub className="text-white hover:text-gray-300" />
+        <FaGithub className="dark:text-white text-black hover:text-green-800" />
       </a>
       <a
         href={personalConfig.social_links.linkedin}
         target="_blank"
         rel="noopener noreferrer"
       >
-        <FaLinkedin className="text-white hover:text-gray-300" />
+        <FaLinkedin className="dark:text-white text-black hover:text-green-800" />
       </a>
       <a
         href={personalConfig.social_links.email}
         target="_blank"
         rel="noopener noreferrer"
       >
-        <MdEmail className="text-white hover:text-gray-300" />
+        <MdEmail className="dark:text-white text-black hover:text-green-800" />
       </a>
-      {/* Add more social media icons as needed */}
     </div>
   );
 }
