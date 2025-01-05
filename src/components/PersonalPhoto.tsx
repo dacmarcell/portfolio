2f9b48aa9db4e847d82144c174fe203ed2c17701
dacmarcell <dacmarcell@gmail.com>
2025-01-05 01:32:17 -0300

    feat: replace PersonalPhoto image with hero design and update dimensions

--- src/components/PersonalPhoto.tsx
+++ src/components/PersonalPhoto.tsx
@@ -3,15 +3,13 @@ import Image from "next/image";
 export default function PersonalPhoto() {
   return (
     <div className="flex items-center justify-center">
-      <div className="w-full sm:w-auto rounded-lg my-5 sm:mx-8 sm:px-8 sm:py-6 dark:bg-opacity-10">
-        <Image
-          width={300}
-          height={300}
-          src="/me.jpg"
-          alt="homem branco com camisa cinza e mochila preta. selfie de frente como o rosto do homem centralizado, barba e bigode médios/pequenos, cabelo curto e preto e sorriso."
-          className="rounded-full border-2 border-solid border-foreground transition-all duration-300 my-5 w-60 h-60 object-cover shadow-lg sm:mt-0 sm:w-72 sm:h-72 hover:scale-105 dark:border-background dark:shadow-none"
-        />
-      </div>
+      <Image
+        width={500}
+        height={500}
+        src="/hero-design.png"
+        alt="Homem branco de camisa cinza e calça preta, com uma mochila preta e um sorriso. No lado esquerdo da cabeça dele tem uma bolha adicionada via edição de imagem com a logo da linguagem de programação Rust. As bolhas do lado direito da cabeça dele se referem às linguagens de programação csharp (c#) e typescript. o fundo da imagem é verde com tonalidade mais puxada pro escuro."
+        className="transition-all duration-300 object-cover shadow-lg hover:scale-105 dark:border-background dark:shadow-none"
+      />
     </div>
   );
 }
