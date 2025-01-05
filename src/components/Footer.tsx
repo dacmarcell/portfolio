516c71d76eefb087cf1a4bc6b8d43e98012ba089
dacmarcell <dacmarcell@gmail.com>
2025-01-05 01:31:49 -0300

    refactor: remove Footer component and its usage in layout

--- src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="dark:bg-gray-900 bg-slate-950 text-white py-4 sticky top-0 z-50">
-      <div className="container mx-auto px-4 flex justify-between items-center text-center md:text-left">
-        <p>Dev Marcell &copy; {new Date().getFullYear()}</p>
-      </div>
-    </footer>
-  );
-}
