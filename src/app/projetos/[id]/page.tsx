3e4d74df28432038dfa2b3e0221a998a2eeea414
dacmarcell <dacmarcell@gmail.com>
2025-01-05 01:32:13 -0300

    style: update link styling and add padding to projetos page

--- src/app/projetos/[id]/page.tsx
+++ src/app/projetos/[id]/page.tsx
@@ -6,6 +6,9 @@ async function getProjectByID(id: number) {
   return isNaN(id) ? null : projects.find((project) => project.id === id);
 }
 
+const linkClassname =
+  "inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-[#008b48] rounded-lg hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800";
+
 export default async function Page({
   params,
 }: {
@@ -18,38 +21,20 @@ export default async function Page({
     return (
       <main>
         <h1>Projeto não encontrado</h1>
-        <Link href="/">Voltar</Link>
+        <Link href="/projetos" className={linkClassname}>
+          Voltar
+        </Link>
       </main>
     );
   }
 
   return (
-    <main>
+    <main className="p-5">
       <Title>{project.title}</Title>
-      <p
-        className="
-        text-foreground 
-        dark:text-white
-        text-black
-        text-lg 
-        sm:text-xl
-        my-4
-      "
-      >
+      <p className="text-foreground dark:text-whitetext-black text-lg sm:text-xl my-4">
         {project.description}
       </p>
-      <Link
-        href="/"
-        className="
-        text-foreground 
-        dark:text-white
-        text-black
-        text-lg 
-        sm:text-xl
-        my-4
-        hover:underline
-      "
-      >
+      <Link href="/projetos" className={linkClassname}>
         Voltar
       </Link>
     </main>
