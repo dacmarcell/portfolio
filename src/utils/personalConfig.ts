40f0c2efdaf6dbd52f11e72193fca30164c63307
dacmarcell <dacmarcell@gmail.com>
2025-01-05 19:31:54 -0300

    feat: add email to personalConfig for contact information

--- src/utils/personalConfig.ts
+++ src/utils/personalConfig.ts
@@ -1,4 +1,5 @@
 const personalConfig = {
+  email: "devmarcell@protonmail.com",
   social_links: {
     github: "https://github.com/dacmarcell",
     linkedin: "https://linkedin.com/in/marcell-dactes",
