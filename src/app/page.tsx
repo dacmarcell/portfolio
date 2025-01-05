3d54400bb82910aad01744697ac4ca55dfbc7502
dacmarcell <dacmarcell@gmail.com>
2025-01-05 20:34:13 -0300

    refactor: replace HeroSection with Presentation, Informations, and PersonalPhoto components in Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,17 @@
-import HeroSection from "@/components/HeroSection/HeroSection";
+import Informations from "@/components/Home/Informations";
+import PersonalPhoto from "@/components/Home/PersonalPhoto";
+import Presentation from "@/components/Home/Presentation";
 
 export default function Home() {
   return (
     <main>
-      <HeroSection />
-      {/* <Welcome />
-      <Line />
-      <AboutMe />
-      <Line />
-      <PersonalPhoto />
-      <Line />
-      <ProjectsList onlyFavorites /> */}
+      <div className="grid grid-cols-1 md:grid-cols-2 h-screen">
+        <div className="flex flex-col justify-center md:items-start md:pl-20">
+          <Presentation />
+          <Informations />
+        </div>
+        <PersonalPhoto />
+      </div>
     </main>
   );
 }
