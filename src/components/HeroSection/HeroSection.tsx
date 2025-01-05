9cf4c7ad98a091839f8790e94838a31742c827e0
dacmarcell <dacmarcell@gmail.com>
2025-01-05 19:32:01 -0300

    feat: integrate Informations component into HeroSection for enhanced contact display

--- src/components/HeroSection/HeroSection.tsx
+++ src/components/HeroSection/HeroSection.tsx
@@ -1,11 +1,13 @@
+import Informations from "./Informations";
 import PersonalPhoto from "./PersonalPhoto";
 import Presentation from "./Presentation";
 
 export default function HeroSection() {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 h-screen">
-      <div className="flex flex-col justify-center items-center md:items-start md:pl-20">
+      <div className="flex flex-col justify-center md:items-start md:pl-20">
         <Presentation />
+        <Informations />
       </div>
       <PersonalPhoto />
     </div>
