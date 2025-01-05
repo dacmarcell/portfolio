da26bc385c56b409126c59ea4e6c8e96b6579fdd
dacmarcell <dacmarcell@gmail.com>
2025-01-04 23:36:19 -0300

    style: adjust margin in Line component for improved spacing

--- src/components/ui/Line.tsx
+++ src/components/ui/Line.tsx
@@ -2,16 +2,15 @@ export default function Line() {
   return (
     <hr
       className="
-          w-1/2 
-          my-8 
-          border-t-2 
-          border-solid 
-          border-foreground 
-          dark:border-background 
-          rounded-full 
-          sm:w-1/4 
-          sm:my-12 
+          w-1/2
+          border-t-2
+          border-solid
+          border-foreground
+          dark:border-background
+          rounded-full
+          sm:w-1/4
           dark:border-opacity-50
+          my-5
         "
     />
   );
