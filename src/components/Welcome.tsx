9d204aed2bc1f717dc1642c690d153cb798c7003
dacmarcell <dacmarcell@gmail.com>
2025-01-04 23:35:09 -0300

    Add Welcome component with introductory message for users

---
+++ src/components/Welcome.tsx
@@ -0,0 +1,8 @@
+export default function Welcome() {
+  return (
+    <h1 className="text-2xl sm:text-3xl text-center sm:text-left">
+      Olá! seja bem-vindo(a). por aqui você vai encontrar um pouco sobre mim,
+      meus projetos de programação e algumas informações interessantes!
+    </h1>
+  );
+}
