fd788fc28f550d945c379e506c5d8a13d57fae70
dacmarcell <dacmarcell@gmail.com>
2025-01-05 00:27:37 -0300

    feat: replace placeholder with PersonalPhoto component in HeroSection

--- src/components/HeroSection/HeroSection.tsx
+++ src/components/HeroSection/HeroSection.tsx
@@ -1,12 +1,11 @@
+import PersonalPhoto from "../PersonalPhoto";
 import Presentation from "./Presentation";
 
 export default function HeroSection() {
   return (
     <div className="grid grid-cols-2 h-screen">
       <Presentation />
-      <div className="bg-blue-500 flex items-center justify-center">
-        <p className="text-white text-xl">Metade Direita</p>
-      </div>
+      <PersonalPhoto />
     </div>
   );
 }
