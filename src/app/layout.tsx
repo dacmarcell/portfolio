ca2ae148a17d6b21c781f27159242b114dc974c6
dacmarcell <dacmarcell@gmail.com>
2025-01-05 00:01:11 -0300

    style: reduce padding in layout component for improved spacing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Header />
-        <div className="flex flex-col items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] dark:bg-slate-950">
+        <div className="min-h-screen p-5 font-[family-name:var(--font-geist-sans)] dark:bg-slate-950">
           {children}
         </div>
         <Footer />
