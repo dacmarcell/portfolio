c013269ea21d9e16a3383ec4ee738fda98ee79e7
dacmarcell <dacmarcell@gmail.com>
2025-01-04 23:35:21 -0300

    Replace welcome message in Home component with Welcome component for improved structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,12 @@ import AboutMe from "@/components/AboutMe";
 import PersonalPhoto from "@/components/PersonalPhoto";
 import ProjectsList from "@/components/ProjectsList";
 import Line from "@/components/ui/Line";
+import Welcome from "@/components/Welcome";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center">
-      <h1 className="text-2xl sm:text-3xl text-center sm:text-left">
-        Olá! seja bem-vindo(a). por aqui você vai encontrar um pouco sobre mim,
-        meus projetos de programação e algumas informações interessantes!
-      </h1>
+      <Welcome />
       <Line />
       <AboutMe />
       <Line />
