2e691bc93f5aec28a7886ee86daa7818c9a80893
dacmarcell <dacmarcell@gmail.com>
2025-01-10 18:43:49 -0300

    style: update background color and text color for improved visibility in Informations component

--- src/components/Home/Informations.tsx
+++ src/components/Home/Informations.tsx
@@ -7,16 +7,16 @@ const copyToClipboard = (text: string) => navigator.clipboard.writeText(text);
 
 export default function Informations() {
   return (
-    <div className="flex flex-col justify-center p-10 bg-slate-200/[0.5] dark:bg-green-600/[0.5] md:rounded-3xl md:mr-4">
-      <h1 className="font-semibold text-xl">Contato</h1>
+    <div className="flex flex-col justify-center p-10 bg-slate-200/[0.5] dark:bg-white md:rounded-3xl  w-full md:w-80 md:mr-4 md:mt-10">
+      <h1 className="font-semibold text-xl dark:text-black">Contato</h1>
       <div className="inline-flex items-center group">
         <button
-          className="text-md text-center text-black rounded-lg hover:underline focus:ring-4 focus:outline-none focus:ring-green-300 dark:bg-green-600 p-1 mt-2"
+          className="text-md text-center text-black rounded-lg hover:underline focus:ring-4 focus:outline-none focus:ring-green-300 p-1 mt-2"
           onClick={() => copyToClipboard(personalConfig.email)}
         >
           {personalConfig.email}
         </button>
-        <FaRegCopy className="ml-2 hidden text-black dark:text-white group-hover:inline" />
+        <FaRegCopy className="ml-2 hidden text-black group-hover:inline" />
       </div>
     </div>
   );
