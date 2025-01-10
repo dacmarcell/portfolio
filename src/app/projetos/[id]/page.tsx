111b0dd97af7918b973195617f6c0307bbaf7ea3
dacmarcell <dacmarcell@gmail.com>
2025-01-10 02:38:57 -0300

    style: enhance project details page layout and styling

--- src/app/projetos/[id]/page.tsx
+++ src/app/projetos/[id]/page.tsx
@@ -17,24 +17,40 @@ export default async function Page({
 
   if (!project) {
     return (
-      <main>
+      <main className="flex flex-col items-center justify-center min-h-screen bg-dark">
         <CustomLink href="/projetos">Voltar</CustomLink>
-        <h1>Projeto não encontrado</h1>
+        <h1 className="text-3xl font-bold text-red-500">
+          Projeto não encontrado
+        </h1>
       </main>
     );
   }
 
   return (
-    <main className="p-5">
-      <CustomLink href="/projetos">Voltar</CustomLink>
-      <Title>{project.title}</Title>
-      <p className="text-foreground dark:text-whitetext-black text-lg sm:text-xl my-4">
-        {project.description}
-      </p>
-      <div className="flex flex-wrap gap-2">
-        {project.technologies.map((technology) => (
-          <Chip key={technology} element={technology} />
-        ))}
+    <main className="p-5 flex flex-col items-center">
+      <CustomLink href="/projetos">
+        <button className="px-4 py-2 bg-green-600 text-white font-medium rounded hover:bg-green-700">
+          Voltar
+        </button>
+      </CustomLink>
+      <div className="bg-dark text-foreground rounded-lg shadow-lg p-6 w-full max-w-4xl">
+        <Title>{project.title}</Title>
+        <p className="text-lg text-center text-gray-400 mb-6">
+          {project.description}
+        </p>
+        <div className="flex justify-center flex-wrap gap-2 mb-6">
+          {project.technologies.map((technology) => (
+            <Chip key={technology} element={technology} />
+          ))}
+        </div>
+        <div className="flex justify-center gap-4">
+          <a
+            href={project.url || "#"}
+            className="px-4 py-2 bg-blue-600 text-white font-medium rounded hover:bg-blue-700"
+          >
+            Saiba mais
+          </a>
+        </div>
       </div>
     </main>
   );
