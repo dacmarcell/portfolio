9bbd168ed8235238148b3a06a95468d4e9fe95f7
dacmarcell <dacmarcell@gmail.com>
2025-01-10 18:20:50 -0300

    style: improve PreviewButton component to fetch site screenshot only if not already loaded

--- src/components/PreviewButton.tsx
+++ src/components/PreviewButton.tsx
@@ -18,14 +18,14 @@ const PreviewButton = ({ project }: PreviewButtonProps) => {
   const [error, setError] = useState("");
 
   const fetchPreviewData = async () => {
-    try {
-      const encodedURI = encodeURIComponent(project.url);
+    if (siteScreenshot) return;
 
+    try {
+      setIsLoading(true);
+      const encodedURI = encodeURIComponent(project.url);
       const response = await fetch(
         `https://opengraph.io/api/1.1/screenshot/${encodedURI}?accept_lang=auto&quality=80&dimensions=md&app_id=${process.env.NEXT_PUBLIC_OPEN_GRAPH_API_KEY}`
-      ).finally(() => {
-        setIsLoading(false);
-      });
+      );
 
       const data: { message: string; screenshotUrl: string } =
         await response.json();
@@ -33,6 +33,8 @@ const PreviewButton = ({ project }: PreviewButtonProps) => {
       setSiteScreenshot(data.screenshotUrl);
     } catch (_) {
       setError("Failed to fetch site screenshot");
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -40,9 +42,17 @@ const PreviewButton = ({ project }: PreviewButtonProps) => {
     if (isSiteScreenshotBoxOpen) return;
 
     setIsSiteScreenshotBoxOpen(true);
-    setIsLoading(true);
-
     await fetchPreviewData();
+  };
+
+  const handleMouseEnter = async () => {
+    if (!isSiteScreenshotBoxOpen) {
+      setIsSiteScreenshotBoxOpen(true);
+
+      if (!siteScreenshot) {
+        await fetchPreviewData();
+      }
+    }
   };
 
   return (
@@ -50,7 +60,7 @@ const PreviewButton = ({ project }: PreviewButtonProps) => {
       <a
         target="_blank"
         href={project.url}
-        onMouseEnter={async () => await handleOpenSiteScreenshotBox()}
+        onMouseEnter={handleMouseEnter}
         className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-[#008b48] rounded-lg hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
       >
         <span>Saiba mais</span>
