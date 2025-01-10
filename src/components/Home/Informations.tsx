b064ec3a04ffe3bff3e0a0a820e78c6fb7e765b2
dacmarcell <dacmarcell@gmail.com>
2025-01-10 02:00:07 -0300

    style: update Informations component styling for improved dark mode support

--- src/components/Home/Informations.tsx
+++ src/components/Home/Informations.tsx
@@ -7,11 +7,11 @@ const copyToClipboard = (text: string) => navigator.clipboard.writeText(text);
 
 export default function Informations() {
   return (
-    <div className="flex flex-col justify-center p-10 bg-slate-200/[0.5] md:rounded-3xl md:mr-4">
+    <div className="flex flex-col justify-center p-10 bg-slate-200/[0.5] dark:bg-green-600/[0.5] md:rounded-3xl md:mr-4">
       <h1 className="font-semibold text-xl">Contato</h1>
       <div className="inline-flex items-center group">
         <button
-          className="text-md text-center text-black rounded-lg hover:underline focus:ring-4 focus:outline-none focus:ring-green-300 dark:bg-green-600"
+          className="text-md text-center text-black rounded-lg hover:underline focus:ring-4 focus:outline-none focus:ring-green-300 dark:bg-green-600 p-1 mt-2"
           onClick={() => copyToClipboard(personalConfig.email)}
         >
           {personalConfig.email}
