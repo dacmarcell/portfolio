2da062e7d1dab3488fb8f737439dd1565e03c51c
dacmarcell <dacmarcell@gmail.com>
2025-01-16 00:26:33 -0300

    fix: update CustomLink background color for improved visibility

--- src/components/ui/CustomLink.tsx
+++ src/components/ui/CustomLink.tsx
@@ -10,7 +10,7 @@ export default function CustomLink(props: CustomLinkProps) {
   return (
     <Link
       href={href}
-      className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-[#008b48] rounded-lg hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+      className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-green-700 rounded-lg hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
     >
       {children}
     </Link>
