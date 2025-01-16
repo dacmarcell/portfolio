171d680fc49d9f732f6d2ecd5c14113d368facc7
dacmarcell <dacmarcell@gmail.com>
2025-01-16 01:57:44 -0300

    feat: wrap Title component in FadeIn for smoother rendering

--- src/components/ui/Title.tsx
+++ src/components/ui/Title.tsx
@@ -1,8 +1,16 @@
+"use client";
+
+import FadeIn from "../motion/FadeIn";
+
 interface TitleProps {
   children: React.ReactNode;
 }
 
 export default function Title(props: TitleProps) {
   const { children } = props;
-  return <h1 className="text-4xl font-semibold my-8">{children}</h1>;
+  return (
+    <FadeIn>
+      <h1 className="text-4xl font-semibold my-8">{children}</h1>
+    </FadeIn>
+  );
 }
