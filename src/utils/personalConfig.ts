d265c9eea42039a843a280fb0ba9df3c1add7e6b
dacmarcell <dacmarcell@gmail.com>
2025-01-15 22:46:58 -0300

    feat: add Gumroad link to personalConfig for easier access

--- src/utils/personalConfig.ts
+++ src/utils/personalConfig.ts
@@ -1,5 +1,6 @@
 const personalConfig = {
   email: "devmarcell@protonmail.com",
+  gumroad: "https://devmarcell.gumroad.com/",
   social_links: {
     github: "https://github.com/dacmarcell",
     linkedin: "https://linkedin.com/in/marcell-dactes",
