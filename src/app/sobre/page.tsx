d183e3d2202fafe6c5cc227b66d13c7a828062a9
devmarcell <dacmarcell@gmail.com>
2025-02-08 16:22:26 -0300

    refactor: organiza componentes e estilização

--- src/app/sobre/page.tsx
+++ src/app/sobre/page.tsx
@@ -8,16 +8,17 @@ import Line from "@/components/ui/Line";
 export default function Page() {
   return (
     <>
-      <div className="flex flex-row p-5">
-        <div className="m-10">
+      <div className="flex flex-row">
+        <FadeIn className="ml-10 mt-10 mr-10">
           <DynamicBlurImage
+            isRounded
             width={500}
             height={500}
             src="https://avatars.githubusercontent.com/u/83461178?v=4"
           />
-        </div>
+        </FadeIn>
         <FadeIn>
-          <p className="text-lg text-justify m-10">
+          <p className="mt-20 text-lg text-start">
             Bacharelando em Ciência da Computação e Desenvolvedor Web FullStack
             com mais de um ano de experiência. Tenho paixão por escrever código
             limpo e eficiente, seguindo princípios como DRY, SOLID, KISS e Clean
@@ -31,9 +32,9 @@ export default function Page() {
           </p>
         </FadeIn>
       </div>
-      <div className="flex flex-row p-5">
+      <div className="flex flex-row">
         <FadeIn>
-          <p className="m-10 text-lg text-end">
+          <p className="mt-10 text-lg text-end">
             Atualmente, trabalho com o ecossistema PHP, especialmente com
             Laravel, e sou proficiente em diversas linguagens, como C#,
             Typescript, Rust e Python. Também tenho experiência com frameworks
@@ -42,14 +43,14 @@ export default function Page() {
             habilidades.
           </p>
         </FadeIn>
-        <div className="m-10">
+        <FadeIn className="ml-10 mr-10">
           <DynamicBlurImage
             isRounded
             width={500}
             height={500}
             src="/hero-design.png"
           />
-        </div>
+        </FadeIn>
       </div>
       <Line />
       <ExperiencesList />
--- src/components/motion/FadeIn.tsx
+++ src/components/motion/FadeIn.tsx
@@ -1,8 +1,14 @@
 import { motion } from "motion/react";
 
-export default function FadeIn({ children }: { children: React.ReactNode }) {
+interface FadeInProps {
+  children: React.ReactNode;
+  className?: string;
+}
+
+export default function FadeIn({ children, className }: FadeInProps) {
   return (
     <motion.div
+      className={className}
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ duration: 0.5 }}
