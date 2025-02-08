adb5c6c6e8195234cc8648b1e6a0da4d184ee19c
devmarcell <dacmarcell@gmail.com>
2025-02-08 05:32:30 -0300

    feat: update personal description in Sobre page for clarity and detail

--- src/app/sobre/page.tsx
+++ src/app/sobre/page.tsx
@@ -16,18 +16,23 @@ export default function Page() {
         <div className="flex items-center justify-center text-dark rounded-md p-4">
           <FadeIn>
             <p className="text-lg">
-              Bacharelando em Ciência da Computação, Desenvolvedor Web FullStack
-              com aproximadamente 1 ano de experiência. Aprecio conceitos como
-              DRY, SOLID, KISS e Clean Code durante a codificação de aplicações,
-              com foco no desenvolvimento de aplicações escaláveis e
-              performáticas, utilizando tecnologias da atualidade. Como um bom
-              Júnior, também adoro experimentar novas tecnologias. Atualmente
-              trabalho com o ecossistema Typescript com foco na Web, utilizando
-              Next.js e Node.js. No back-end que é minha stack, atualmente
-              utilizo C# e .NET para criação de aplicações. Também tenho
-              projetos pessoais em Rust, Python e outras linguagens. Tenho um
-              gato chamado Django e amo pizza. Sou curioso e inquieto. Sempre
-              estudando algo novo ou fazendo algum projeto.
+              Bacharelando em Ciência da Computação e Desenvolvedor Web
+              FullStack com mais de um ano de experiência. Tenho paixão por
+              escrever código limpo e eficiente, seguindo princípios como DRY,
+              SOLID, KISS e Clean Code, sempre com foco na criação de aplicações
+              escaláveis e performáticas utilizando tecnologias modernas.
+              <br />
+              Atualmente, trabalho com o ecossistema PHP, especialmente com
+              Laravel, e sou proficiente em diversas linguagens, como C#,
+              Typescript, Rust e Python. Também tenho experiência com frameworks
+              como Next.js, .NET e Quasar. Como um desenvolvedor Júnior curioso,
+              adoro explorar novas tecnologias e aprimorar constantemente minhas
+              habilidades.
+              <br />
+              Nas horas vagas, estou sempre estudando algo novo ou desenvolvendo
+              projetos pessoais. Tenho um gato chamado Django, sou fã de pizza e
+              movido pela inquietude de quem está sempre em busca de
+              aprendizado.Bacharelando
             </p>
           </FadeIn>
         </div>
