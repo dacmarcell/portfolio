00707c97bfe3f0f35de39c7fc98541b479251fc1
devmarcell <dacmarcell@gmail.com>
2025-02-08 16:13:40 -0300

    refactor: use dynamic blur image component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,18 @@
 import Informations from "@/components/Home/Informations";
-import PersonalPhoto from "@/components/Home/PersonalPhoto";
+import DynamicBlurImage from "@/components/Home/DynamicBlurImage";
 import Presentation from "@/components/Home/Presentation";
 
 export default function Home() {
   return (
     <main>
-      <div className="grid grid-cols-1 md:grid-cols-2 h-screen">
+      <div className="grid grid-cols-1 md:grid-cols-2 mt-10">
         <div className="flex flex-col justify-center md:items-start md:pl-20">
           <Presentation />
+          {/* div abaixo aparece somente em celular */}
           <div className="md:hidden w-full h-10 bg-[#068155]" />
           <Informations />
         </div>
-        <PersonalPhoto />
+        <DynamicBlurImage src="/hero-design.png" height={400} width={500} />
       </div>
     </main>
   );
--- src/app/sobre/page.tsx
+++ src/app/sobre/page.tsx
@@ -1,40 +1,54 @@
 "use client";
 
-import ClassroomPhoto from "@/components/About/ClassroomPhoto";
 import ExperiencesList from "@/components/About/ExperiencesList";
+import DynamicBlurImage from "@/components/Home/DynamicBlurImage";
 import FadeIn from "@/components/motion/FadeIn";
 import Line from "@/components/ui/Line";
 
 export default function Page() {
   return (
     <>
-      <div className="grid grid-cols-2 p-5">
-        <div className="flex items-center justify-center text-white rounded-md p-4">
-          <ClassroomPhoto />
+      <div className="flex flex-row p-5">
+        <div className="m-10">
+          <DynamicBlurImage
+            width={500}
+            height={500}
+            src="https://avatars.githubusercontent.com/u/83461178?v=4"
+          />
         </div>
-
-        <div className="flex items-center justify-center text-dark rounded-md p-4">
-          <FadeIn>
-            <p className="text-lg">
-              Bacharelando em Ciência da Computação e Desenvolvedor Web
-              FullStack com mais de um ano de experiência. Tenho paixão por
-              escrever código limpo e eficiente, seguindo princípios como DRY,
-              SOLID, KISS e Clean Code, sempre com foco na criação de aplicações
-              escaláveis e performáticas utilizando tecnologias modernas.
-              <br />
-              Atualmente, trabalho com o ecossistema PHP, especialmente com
-              Laravel, e sou proficiente em diversas linguagens, como C#,
-              Typescript, Rust e Python. Também tenho experiência com frameworks
-              como Next.js, .NET e Quasar. Como um desenvolvedor Júnior curioso,
-              adoro explorar novas tecnologias e aprimorar constantemente minhas
-              habilidades.
-              <br />
-              Nas horas vagas, estou sempre estudando algo novo ou desenvolvendo
-              projetos pessoais. Tenho um gato chamado Django, sou fã de pizza e
-              movido pela inquietude de quem está sempre em busca de
-              aprendizado.Bacharelando
-            </p>
-          </FadeIn>
+        <FadeIn>
+          <p className="text-lg text-justify m-10">
+            Bacharelando em Ciência da Computação e Desenvolvedor Web FullStack
+            com mais de um ano de experiência. Tenho paixão por escrever código
+            limpo e eficiente, seguindo princípios como DRY, SOLID, KISS e Clean
+            Code, sempre com foco na criação de aplicações escaláveis e
+            performáticas utilizando tecnologias modernas.
+            <br />
+            Nas horas vagas, estou sempre estudando algo novo ou desenvolvendo
+            projetos pessoais. Tenho um gato chamado Django, sou fã de pizza e
+            movido pela inquietude de quem está sempre em busca de
+            aprendizado.Bacharelando
+          </p>
+        </FadeIn>
+      </div>
+      <div className="flex flex-row p-5">
+        <FadeIn>
+          <p className="m-10 text-lg text-end">
+            Atualmente, trabalho com o ecossistema PHP, especialmente com
+            Laravel, e sou proficiente em diversas linguagens, como C#,
+            Typescript, Rust e Python. Também tenho experiência com frameworks
+            como Next.js, .NET e Quasar. Como um desenvolvedor Júnior curioso,
+            adoro explorar novas tecnologias e aprimorar constantemente minhas
+            habilidades.
+          </p>
+        </FadeIn>
+        <div className="m-10">
+          <DynamicBlurImage
+            isRounded
+            width={500}
+            height={500}
+            src="/hero-design.png"
+          />
         </div>
       </div>
       <Line />
