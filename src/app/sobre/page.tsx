72ed311ce33d9271da068cf1b2b50a1741177983
devmarcell <dacmarcell@gmail.com>
2025-02-08 20:47:05 -0300

    feat: enhance Sobre page with new layout and motion effects, highlighting SemComp 2024 achievement

--- src/app/sobre/page.tsx
+++ src/app/sobre/page.tsx
@@ -4,6 +4,7 @@ import ExperiencesList from "@/components/About/ExperiencesList";
 import DynamicBlurImage from "@/components/Home/DynamicBlurImage";
 import FadeIn from "@/components/motion/FadeIn";
 import Line from "@/components/ui/Line";
+import { motion } from "motion/react";
 
 export default function Page() {
   return (
@@ -11,14 +12,13 @@ export default function Page() {
       <div className="flex flex-row">
         <FadeIn className="ml-10 mt-10 mr-10">
           <DynamicBlurImage
-            isRounded
             width={500}
             height={500}
             src="https://avatars.githubusercontent.com/u/83461178?v=4"
           />
         </FadeIn>
         <FadeIn>
-          <p className="mt-20 text-lg text-start">
+          <p className="mr-10 mt-10 text-lg text-start rounded-lg p-10 border border-black ">
             Bacharelando em Ciência da Computação e Desenvolvedor Web FullStack
             com mais de um ano de experiência. Tenho paixão por escrever código
             limpo e eficiente, seguindo princípios como DRY, SOLID, KISS e Clean
@@ -33,9 +33,9 @@ export default function Page() {
         </FadeIn>
       </div>
 
-      <div className="flex flex-row">
+      <div className="flex flex-row mt-10">
         <FadeIn>
-          <p className="mt-10 text-lg text-end">
+          <p className="ml-10 mt-10 text-lg text-end rounded-lg border border-black p-10">
             Atualmente, trabalho com o ecossistema PHP, especialmente com
             Laravel, e sou proficiente em diversas linguagens, como C#,
             Typescript, Rust e Python. Também tenho experiência com frameworks
@@ -45,16 +45,18 @@ export default function Page() {
           </p>
         </FadeIn>
         <FadeIn className="ml-10 mr-10">
-          <DynamicBlurImage
-            isRounded
-            width={500}
-            height={500}
-            src="/hero-design.png"
-          />
+          <DynamicBlurImage width={500} height={500} src="/me.jpg" />
         </FadeIn>
       </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-8 p-10 items-center">
+      <motion.p
+        className="text-md p-2 border border-black rounded-lg bg-grey-500/[0.8] w-1/2 m-auto mt-10 text-center text-bold"
+        whileHover={{ scale: 1.1 }}
+      >
+        3° lugar no hackathon SemComp UFBA 2024
+      </motion.p>
+
+      <div className="m-10 grid grid-cols-1 md:grid-cols-3 gap-8">
         <FadeIn className="border-2 border-black rounded-lg overflow-hidden">
           <DynamicBlurImage
             width={500}
@@ -63,18 +65,14 @@ export default function Page() {
           />
         </FadeIn>
 
-        <FadeIn className="md:col-span-1 text-lg text-start">
-          <p className="mt-4 md:mt-0">
-            <b>3° lugar no hackathon SemComp UFBA 2024</b>
-            <br />
-            Foi uma grande honra participar do Hackathon SEMCOMP UFBA 2024. Por
-            lá desenvolvemos uma solução que leva tecnologia e informação para
-            as comunidades marginalizadas.
-            <br />
+        <FadeIn className="text-lg">
+          <p className="mt-10 border border-black p-10 rounded-lg">
+            Foi uma grande honra participar do Hackathon{" "}
+            <b>SEMCOMP UFBA 2024</b>. Por lá desenvolvemos uma solução que leva
+            tecnologia e informação para as comunidades marginalizadas.
             Arquitetura idealizada por mim visando foco em públicos
-            marginalizados com dificuldade de acesso à internet.
-            <br />
-            Com essa solução, conseguimos o terceiro lugar na competição. Foi um
+            marginalizados com dificuldade de acesso à internet. Com essa
+            solução, conseguimos o <b>terceiro lugar na competição</b>. Foi um
             desafio muito proveitoso para a equipe como um todo, uma ótima
             experiência!
           </p>
