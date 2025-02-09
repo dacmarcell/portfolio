83d9692adbdd89ca2571c1924ce6f7f15a614151
devmarcell <dacmarcell@gmail.com>
2025-02-08 23:14:57 -0300

    feat: add back navigation icon to project page link

--- src/app/projetos/[id]/page.tsx
+++ src/app/projetos/[id]/page.tsx
@@ -1,6 +1,7 @@
 import { projects } from "../../../../public/mocks/projects.json";
 import CustomLink from "@/components/ui/CustomLink";
 import ProjectPage from "@/components/ProjectPage";
+import { BsArrowLeft } from "react-icons/bs";
 
 async function getProjectByID(id: number) {
   return isNaN(id) ? null : projects.find((project) => project.id === id);
@@ -17,7 +18,10 @@ export default async function Page({
   if (!project) {
     return (
       <main className="flex flex-col items-center justify-center min-h-screen bg-dark">
-        <CustomLink href="/projetos">Voltar</CustomLink>
+        <CustomLink href="/projetos">
+          <BsArrowLeft className="w-5 h-5 mr-2" />
+          Voltar
+        </CustomLink>
         <h1 className="text-3xl font-bold text-red-500">
           Projeto não encontrado
         </h1>
