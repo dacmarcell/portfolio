bb627b0c865b4b961b76ee466e75398184095f53
devmarcell <dacmarcell@gmail.com>
2025-02-08 22:24:22 -0300

    feat: replace ProjectsList with dynamic Card components and add Title for project section

--- src/app/projetos/page.tsx
+++ src/app/projetos/page.tsx
@@ -1,9 +1,26 @@
-import ProjectsList from "@/components/ProjectsList";
+import Title from "@/components/ui/Title";
+import Card from "@/components/Card";
+
+import { projects } from "../../../public/mocks/projects.json";
 
 export default function Page() {
   return (
-    <main className="p-5">
-      <ProjectsList />
+    <main className="p-10">
+      <Title className="text-white">Projetos</Title>
+      <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
+        {projects.map((project) => {
+          return (
+            <Card
+              isProject
+              key={project.id}
+              title={project.title}
+              id={project.id.toString()}
+              description={project.description}
+              technologies={project.technologies}
+            />
+          );
+        })}
+      </div>
     </main>
   );
 }
