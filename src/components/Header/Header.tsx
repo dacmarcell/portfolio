e0e077743e463b8577649f6e28ac2336183f02a4
devmarcell <dacmarcell@gmail.com>
2025-02-08 22:56:50 -0300

    feat: update Header component to use dynamic navigation and improve layout

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,31 +1,37 @@
 import Link from "next/link";
-import Logo from "./Logo";
-import NavigationLinks from "./NavigationLinks";
 import SocialIcons from "./SocialIcons";
 import personalConfig from "@/utils/personalConfig";
+import { navigation } from "../../../public/mocks/navigation.json";
+import DynamicBlurImage from "../DynamicBlurImage";
 
 export default function Header() {
   return (
-    <header className="bg-gray-950 text-white py-4 sticky top-0 z-50 w-full px-5">
-      <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
-        <Logo />
-        <nav>
-          <ul className="flex gap-x-6">
-            <NavigationLinks />
-            <li>
-              <Link
-                href={personalConfig.gumroad}
-                className="hover:text-green-800 hover:underline text-white"
-                target="_blank"
-              >
-                Loja
-              </Link>
-            </li>
-          </ul>
-        </nav>
-        <div>
-          <SocialIcons />
-        </div>
+    <header className="py-4 sticky top-0 z-50 px-5 container mx-auto flex flex-col md:flex-row justify-between items-center">
+      <DynamicBlurImage src="/light-logo.png" width={110} />
+      <nav>
+        <ul className="flex gap-x-6">
+          {navigation.map((item) => {
+            return (
+              <li key={item.id}>
+                <Link href={item.endpoint} className="hover:underline">
+                  {item.formattedName}
+                </Link>
+              </li>
+            );
+          })}
+          <li>
+            <Link
+              href={personalConfig.gumroad}
+              className="hover:underline"
+              target="_blank"
+            >
+              Loja
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <div>
+        <SocialIcons />
       </div>
     </header>
   );
