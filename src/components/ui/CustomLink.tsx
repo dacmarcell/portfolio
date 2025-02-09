731ecf62b6700d950db31151fbc8a4e1f69fd7ec
devmarcell <dacmarcell@gmail.com>
2025-02-08 22:56:58 -0300

    feat: refactor CustomLink component for improved destructuring and styling consistency

--- src/components/ui/CustomLink.tsx
+++ src/components/ui/CustomLink.tsx
@@ -5,12 +5,11 @@ interface CustomLinkProps {
   href: string;
 }
 
-export default function CustomLink(props: CustomLinkProps) {
-  const { href, children } = props;
+export default function CustomLink({ children, href }: CustomLinkProps) {
   return (
     <Link
       href={href}
-      className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-green-700 rounded-lg hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+      className="inline-flex items-center px-3 py-2 text-sm font-medium text-center bg-green-700 rounded-lg hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
     >
       {children}
     </Link>
