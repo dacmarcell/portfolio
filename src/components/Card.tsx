d26de0f5ccd4263a6a77902f6876311efe3a4388
devmarcell <dacmarcell@gmail.com>
2025-02-08 22:56:35 -0300

    feat: enhance Card component with gradient background and updated button styling

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import Button from "./ui/Button";
 import ArrowIcon from "./ui/ArrowIcon";
 import { useRepositoryContext } from "@/contexts/RepositoryContext";
 import Chip from "./ui/Chip";
@@ -37,22 +36,20 @@ export default function Card(props: CardProps) {
   };
 
   return (
-    <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-      <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+    <div className="group max-w-sm p-6 bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 border border-gray-700 rounded-xl shadow-xl transition transform hover:-translate-y-2 hover:shadow-2xl">
+      {/* Overlay com brilho */}
+      <div className="inset-0 bg-gradient-to-t from-transparent to-gray-900 opacity-30 rounded-xl transition group-hover:opacity-40"></div>
+
+      <h5 className="mb-3 text-2xl font-bold tracking-tight text-white">
         <FadeIn>{title}</FadeIn>
       </h5>
+
       <FadeIn>
-        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-          {description}
-        </p>
+        <p className="mb-4 font-normal text-gray-300">{description}</p>
       </FadeIn>
-      <FadeIn>
-        <Button handleClick={handleClick}>
-          Saiba mais
-          <ArrowIcon />
-        </Button>
-      </FadeIn>
-      {technologies && technologies.length > 0 ? (
+
+      {/* Tecnologias usadas */}
+      {technologies && technologies.length > 0 && (
         <div className="flex flex-wrap gap-2 mt-4">
           {technologies.map((technology) => (
             <FadeIn key={technology}>
@@ -60,7 +57,15 @@ export default function Card(props: CardProps) {
             </FadeIn>
           ))}
         </div>
-      ) : null}
+      )}
+
+      <button
+        onClick={handleClick}
+        className="bg-green-500 hover:bg-green-600 text-white font-semibold px-4 py-2 rounded-lg transition flex items-center gap-2 mt-10"
+      >
+        Saiba mais
+        <ArrowIcon />
+      </button>
     </div>
   );
 }
