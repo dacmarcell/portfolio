1afaabb5a1e3d7bb2131a4fe78a26102f79df480
devmarcell <dacmarcell@gmail.com>
2025-02-22 12:41:08 -0300

    feat: add date formatting utility using dayjs

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,10 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+import dayjs from "dayjs";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const formatDateToDMYHM = (date?: Date) =>
+  dayjs(date).format("DD/MM/YYYY - HH:mm") ?? "-";
