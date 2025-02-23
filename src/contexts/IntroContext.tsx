e76c079edc14050899e9d1f428819da573af3ba0
devmarcell <dacmarcell@gmail.com>
2025-02-23 17:16:50 -0300

    feat: add IntroContext and IntroWrapper for managing introductory state

---
+++ src/contexts/IntroContext.tsx
@@ -0,0 +1,28 @@
+import { createContext, useContext, useState } from "react";
+
+interface IntroContextProps {
+  isIntroFinished: boolean;
+  setIsIntroFinished: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const IntroContext = createContext<IntroContextProps>({} as IntroContextProps);
+
+interface IntroWrapperProps {
+  children: React.ReactNode;
+}
+
+export function IntroWrapper({ children }: IntroWrapperProps) {
+  const [isIntroFinished, setIsIntroFinished] = useState(false);
+
+  return (
+    <IntroContext.Provider value={{ isIntroFinished, setIsIntroFinished }}>
+      {children}
+    </IntroContext.Provider>
+  );
+}
+
+export function useIntroContext() {
+  return useContext(IntroContext);
+}
+
+export default IntroContext;
