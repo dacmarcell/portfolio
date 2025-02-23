50eec178a67d98750dee76836535c80186203b06
devmarcell <dacmarcell@gmail.com>
2025-02-23 17:16:34 -0300

    feat: implement introductory screen with timeout before displaying HomePage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
 "use client";
 
+import Intro from "@/components/Intro";
 import HomePage from "@/pages/home/HomePage";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  return <HomePage />;
+  const [isIntroFinished, setIsIntroFinished] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsIntroFinished(true);
+    }, 10000);
+  });
+
+  return isIntroFinished ? <HomePage /> : <Intro />;
 }
