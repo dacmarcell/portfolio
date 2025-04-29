22c045d90843b4d9c9edef6f9748642532ade45a
devmarcell <devmarcell@protonmail.com>
2025-04-28 22:37:15 -0300

    refactor(RepositoriosPage): comment out ReposList component for future use

--- src/pages/repositorios/RepositoriosPage.tsx
+++ src/pages/repositorios/RepositoriosPage.tsx
@@ -1,9 +1,3 @@
-import ReposList from "@/components/ReposList/ReposList";
-
 export default function RepositoriosPage() {
-  return (
-    <main className="p-5">
-      <ReposList />
-    </main>
-  );
+  return <main className="p-5">{/* <ReposList /> */}</main>;
 }
